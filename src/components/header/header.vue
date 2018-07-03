c6f17eb33919b68ae66230ab81f3cda58cb89b29
zhuqihui <zhuqihui>
2018-07-03 18:21:03 +0800

    头部样式完成

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -12,12 +12,24 @@
         <div class="description">
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
-        <div class="support"></div>
+        <div class="support" v-if="seller.supports">
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+      </div>
+      <div class="support-count" v-if="seller.supports" @click="showDetail">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper"></div>
-      header
-
+    <div class="bulletin-wrapper" @click="showDetail">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
   </div>
 </template>
 
@@ -30,15 +42,31 @@ export default {
   },
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      classMap: ['decrease', 'discount', 'special', 'invoice', 'guarantee'],
+      detailShow: false
+    }
+  },
+  methods: {
+    showDetail() {
+      this.detailShow = true
+    },
+    hideDetail() {
+      this.detailShow = false
     }
   }
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin"
+
   .header
+    position: relative
+    overflow: hidden
+    color: #fff
+    background: rgba(7, 17, 27, 0.5)
     .content-wrapper
+      position: relative
       padding : 24px 12px 18px 24px
       font-size : 0
       .avatar
@@ -50,16 +78,92 @@ export default {
         display : inline-block
         margin-left : 16px
         .title
+          margin: 2px 0 8px 0
           .brand
             display : inline-block
             vertical-align: top
             width: 30px
             height : 18px
             background-size: 30px 18px
-            background-image: url("brand@2x.png")
+            background-repeat : no-repeat
+            bg-image('brand')
           .name
-            display : inline-block
+            margin-left : 6px
             font-size : 16px
+            line-height : 18px
+            font-weight : bold
         .description
+          margin-bottom : 10px
           font-size : 12px
+          line-height : 12px
+        .support
+          .icon
+            display : inline-block
+            vertical-align : top
+            width : 12px
+            height : 12px
+            margin-right : 4px
+            background-size: 12px 12px
+            background-repeat : no-repeat
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+          .text
+            font-size : 10px
+            line-height: 12px
+      .support-count
+        position : absolute
+        right: 12px
+        bottom: 14px
+        padding: 0 8px
+        height: 24px
+        border-radius: 14px
+        background: rgba(0, 0, 0, 0.2)
+        .count
+          line-height: 24px
+          font-size : 10px
+        .icon-keyboard_arrow_right
+          margin-left: 2px
+          font-size : 10px
+    .bulletin-wrapper
+      position: relative
+      height : 28px
+      line-height : 28px
+      padding: 0 22px 0 12px
+      overflow : hidden
+      white-space : nowrap
+      text-overflow : ellipsis
+      background: rgba(7, 17, 27, 0.2)
+      .bulletin-title
+        display : inline-block
+        vertical-align : top
+        width: 22px
+        height: 12px
+        margin-top : 8px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        margin : 0 4px
+        font-size : 10px
+      .icon-keyboard_arrow_right
+        position : absolute
+        right : 12px
+        top : 8px
+        font-size : 10px
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
 </style>
